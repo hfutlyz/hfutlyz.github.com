de2e59cad24b05870c68f489063363d760e470a0
homepc <hfutlyz@gmail.com>
2017-01-04 16:26:38 +0800

    init

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import 'core-js/fn/object/assign';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import TopNav from './components/TopNav';
-
-// Render the main component into the dom
-ReactDOM.render(<TopNav />, document.getElementById('topNav'));
